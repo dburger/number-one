3469c6cdba58dd8548d11b343d60b856eb62aaa8
dburger <dburger@localhost>
2015-01-06 21:29:11 -0800

    Add mixed Colley / Massey examples

--- colley/colley-massey.m
+++ colley/colley-massey.m
@@ -1,14 +1,14 @@
 #!/usr/bin/env octave
 
-% Computes Colley rankings on data as downloaded
-% from http://www.masseyratings.com/data.php
+% Computes "Colleyized Massey" rankings on data as
+% downloaded from http://www.masseyratings.com/data.php
 % in the "Matlab Games" and "Matlab Teams" formats.
 % Team one identifier and score found in columns
 % 3 and 5, team two identifier and score found in
 % columns 6 and 8. Outputs a single overall rating.
 % Example invocation:
 
-% colley.m scores.php teams.php
+% massey-colley.m scores.php teams.php
 
 % For detailed informaiton on the algorithm see
 % chapter three in
@@ -22,7 +22,7 @@ teams = textread(args{2}, '%*d,%s');
 numteams = max(max(games(:,3)), max(games(:,6)));
 
 C = 2 * eye(numteams);
-b = zeros(numteams, 1);
+p = zeros(numteams, 1);
 
 for i = 1:rows(games)
   team1 = games(i, 3);
@@ -36,20 +36,14 @@ for i = 1:rows(games)
   C(team1, team1) += 1;
   C(team2, team2) += 1;
 
+  % In this "Colleyized Massey" the right hand side is
+  % the point differential instead of win loss differential.
   diff = score1 - score2;
-  if (diff > 0)
-    b(team1, 1) += 1;
-    b(team2, 1) -= 1;
-  else
-    b(team1, 1) -= 1;
-    b(team2, 1) += 1;
-  endif
+  p(team1, 1) += diff;
+  p(team2, 1) -= diff;
 endfor
 
-b /= 2;
-b += 1;
-
-r = C \ b;
+r = C \ p;
 
 num = 1:numteams;
 num = num';
--- colley/massey-colley.m
+++ colley/massey-colley.m
@@ -1,14 +1,14 @@
 #!/usr/bin/env octave
 
-% Computes Colley rankings on data as downloaded
-% from http://www.masseyratings.com/data.php
+% Computes Masseyized Colley rankings on data as
+% downloaded from http://www.masseyratings.com/data.php
 % in the "Matlab Games" and "Matlab Teams" formats.
 % Team one identifier and score found in columns
 % 3 and 5, team two identifier and score found in
 % columns 6 and 8. Outputs a single overall rating.
 % Example invocation:
 
-% colley.m scores.php teams.php
+% massey-colley.m scores.php teams.php
 
 % For detailed informaiton on the algorithm see
 % chapter three in
@@ -21,7 +21,9 @@ teams = textread(args{2}, '%*d,%s');
 
 numteams = max(max(games(:,3)), max(games(:,6)));
 
-C = 2 * eye(numteams);
+% In this "Masseyized Colley" we use the M of Massey
+% rather than the C of Colley.
+M = zeros(numteams, numteams);
 b = zeros(numteams, 1);
 
 for i = 1:rows(games)
@@ -31,10 +33,10 @@ for i = 1:rows(games)
   score1 = games(i, 5);
   score2 = games(i, 8);
 
-  C(team1, team2) -= 1;
-  C(team2, team1) -= 1;
-  C(team1, team1) += 1;
-  C(team2, team2) += 1;
+  M(team1, team2) -= 1;
+  M(team2, team1) -= 1;
+  M(team1, team1) += 1;
+  M(team2, team2) += 1;
 
   diff = score1 - score2;
   if (diff > 0)
@@ -49,7 +51,7 @@ endfor
 b /= 2;
 b += 1;
 
-r = C \ b;
+r = M \ b;
 
 num = 1:numteams;
 num = num';
