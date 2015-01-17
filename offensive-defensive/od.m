ce616f16ea27e8e0dc9fbbb42fb29e9ced176807
dburger <dburger@localhost>
2015-01-17 12:21:14 -0800

    add offensive-defensive example

---
+++ offensive-defensive/od.m
@@ -0,0 +1,75 @@
+#!/usr/bin/env octave
+
+% Computes Offensive-Defensive  rankings on data as
+% downloaded from http://www.masseyratings.com/data.php
+% in the "Matlab Games" and "Matlab Teams" formats.
+% Team one identifier and score found in columns
+% 3 and 5, team two identifier and score found in
+% columns 6 and 8. Outputs defensive, offensive, and
+% overall rating.
+% Example invocation:
+
+% od.m scores.php teams.php
+
+% For detailed informaiton on the algorithm see
+% chapter seven in
+% "Who's #1?: The Science of Rating and Ranking."
+
+args = argv();
+
+games = csvread(args{1});
+teams = textread(args{2}, '%*d,%s');
+
+numteams = max(max(games(:,3)), max(games(:,6)));
+
+numgames = zeros(numteams, numteams);
+scores = zeros(numteams, numteams);
+
+for i = 1:rows(games)
+  team1 = games(i, 3);
+  team2 = games(i, 6);
+
+  score1 = games(i, 5);
+  score2 = games(i, 8);
+
+  % Accumulate scores.
+  scores(team1, team2) += score2;
+  scores(team2, team1) += score1;
+
+  numgames(team1, team2) += 1;
+  numgames(team2, team1) += 1;
+endfor
+
+% Change the cumulative scores to averages per game.
+A = scores ./ numgames;
+A(isnan(A)) = 0;
+
+% Calculate the defensive ratings, how many times to converge?
+d = ones(numteams, 1);
+for i = 1:9
+  d = A * (1 ./ (A' * (1 ./ d)));
+endfor
+
+% Then calculate the offensive and overall ratings.
+o = A' * (1 ./ d);
+r = o ./ d;
+
+num = 1:numteams;
+num = num';
+
+r = sortrows([num r], -2);
+o = sortrows([num o], -2);
+d = sortrows([num d], -2);
+
+function output(type, rankings, teams)
+  printf('%s rankings: =====================================\n', type)
+  for i = 1:rows(rankings)
+    printf('%d %s %f\n', i, teams{rankings(i,1)}, rankings(i,2))
+  endfor
+endfunction
+
+output('Overall', r, teams);
+printf('\n')
+output('Offense', o, teams);
+printf('\n')
+output('Defense', d, teams);
