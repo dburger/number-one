aa92d83abe69584b0a97ff6618681a72a314c2d6
David Burger <dburger@google.com>
2021-03-03 12:29:23 -0700

    Add offense / defense ratings for Colleyized Massey.

--- colley/colley-massey.m
+++ colley/colley-massey.m
@@ -22,6 +22,8 @@ teams = textread(args{2}, '%*d,%s');
 numteams = max(max(games(:,3)), max(games(:,6)));
 
 C = 2 * eye(numteams);
+f = zeros(numteams, 1);
+a = zeros(numteams, 1);
 p = zeros(numteams, 1);
 
 for i = 1:rows(games)
@@ -38,6 +40,11 @@ for i = 1:rows(games)
 
   % In this "Colleyized Massey" the right hand side is
   % the point differential instead of win loss differential.
+  f(team1, 1) += score1;
+  a(team1, 1) += score2;
+  f(team2, 1) += score2;
+  a(team2, 1) += score1;
+
   diff = score1 - score2;
   p(team1, 1) += diff;
   p(team2, 1) -= diff;
@@ -45,13 +52,34 @@ endfor
 
 r = C \ p;
 
+% Here we add some offense and defense rating computations. See the tail end
+% of chapter two in
+% "Who's #1?: The Science of Rating and Ranking." where it talks about how
+% to add such ratings to Massey, here we do the same thing for
+% Colleyized Massey.
+
+T = diag(diag(C), numteams, numteams);
+P = -1 * (C - T);
+
+d = (T + P) \ (T * r - f)
+o = r - d;
+
 num = 1:numteams;
 num = num';
 
 r = sortrows([num r], -2);
+o = sortrows([num o], -2);
+d = sortrows([num d], -2);
 
-printf('rankings: =====================================\n')
-for i = 1:rows(r)
-  printf('%d %s %f\n', i, teams{r(i,1)}, r(i,2))
-endfor
+function output(type, rankings, teams)
+  printf('%s rankings: =====================================\n', type)
+  for i = 1:rows(rankings)
+    printf('%d %s %f\n', i, teams{rankings(i,1)}, rankings(i,2))
+  endfor
+endfunction
 
+output('Overall', r, teams);
+printf('\n')
+output('Offense', o, teams);
+printf('\n')
+output('Defense', d, teams);
