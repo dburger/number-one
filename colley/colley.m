d94ed7d4531b1e4f30cfa414f8867feef51366df
David J. Burger <davidburger@gmail.com>
2021-12-07 20:15:09 -0700

    Change colley script to allow weighting.
    Change the colley scrip to allow weighting according to the amount of
    the season that has elapsed.

--- colley/colley.m
+++ colley/colley.m
@@ -8,11 +8,20 @@
 % columns 6 and 8. Outputs a single overall rating.
 % Example invocation:
 
-% colley.m scores.php teams.php
+% colley.m scores.php teams.php [seasonstart seasonend]
+
+% Providing seasonstart and seasonend, in the form of
+% yyyymmdd, will cause the algorithm to apply a weight
+% corresponding to the fraction of the season that has
+% passed. That is a game played when only 1/10 of the
+% season has elapsed will have 1/10 the weight of games
+% played on the last day of the season.
 
 % For detailed informaiton on the algorithm see
 % chapter three in
 % "Who's #1?: The Science of Rating and Ranking."
+
+datetmpl = '%Y%m%d';
 
 args = argv();
 
@@ -23,31 +32,60 @@ fid = fopen(args{2}, 'r');
 teams = textscan(fid, '%*d,%s'){1};
 fclose(fid);
 
+if (length(args) == 4)
+  [seasonstart, nchars] = strptime(args{3}, datetmpl);
+  if (nchars == 0)
+    error('seasonstart does not parse.');
+  endif
+  seasonstart = mktime(seasonstart);
+  [seasonend, nchars] = strptime(args{4}, datetmpl);
+  if (nchars == 0)
+    error('seasonend does not parse.');
+  endif
+  seasonend = mktime(seasonend);
+  seasonlen = seasonend - seasonstart;
+  if (seasonlen <= 0)
+    error('Invalid seasonlen.')
+  end
+endif
+
 numteams = max(max(games(:,3)), max(games(:,6)));
 
 C = 2 * eye(numteams);
 b = zeros(numteams, 1);
 
 for i = 1:rows(games)
+  gamestart = mktime(strptime(num2str(games(i, 2)), datetmpl));
+
   team1 = games(i, 3);
   team2 = games(i, 6);
 
   score1 = games(i, 5);
   score2 = games(i, 8);
 
-  C(team1, team2) -= 1;
-  C(team2, team1) -= 1;
-  C(team1, team1) += 1;
-  C(team2, team2) += 1;
+  weight = 1;
+
+  if (exist('seasonstart', 'var') == 1)
+    offset = gamestart - seasonstart;
+    if (offset < 0)
+      error('Game offset less than 0');
+    endif
+    weight = (gamestart - seasonstart) / seasonlen;
+  endif
+
+  C(team1, team2) -= 1 * weight;
+  C(team2, team1) -= 1 * weight;
+  C(team1, team1) += 1 * weight;
+  C(team2, team2) += 1 * weight;
 
   diff = score1 - score2;
 
   if (diff > 0)
-    b(team1, 1) += 1;
-    b(team2, 1) -= 1;
+    b(team1, 1) += 1 * weight;
+    b(team2, 1) -= 1 * weight;
   elseif (diff < 0)
-    b(team1, 1) -= 1;
-    b(team2, 1) += 1;
+    b(team1, 1) -= 1 * weight;
+    b(team2, 1) += 1 * weight;
   endif
   % if (diff == 0) no change to b is necessary, as we conceptually count it as
   % 1/2 win and 1/2 loss for each team
