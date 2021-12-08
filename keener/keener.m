acfc215737ab47c2a225b9ac4f6c37b071d1f551
David J. Burger <davidburger@gmail.com>
2021-12-07 20:57:53 -0700

    Make keener script optionally weight.
    Modify the keener script to optionally allow for weighting by the
    fraction of the season that has elapsed.

--- keener/keener.m
+++ keener/keener.m
@@ -8,7 +8,14 @@
 % columns 6 and 8. Outputs a single overall rating.
 % Example invocation:
 
-% keener.m scores.php teams.php
+% keener.m scores.php teams.php [seasonstart seasonend]
+
+% Providing seasonstart and seasonend, in the form of
+% yyyymmdd, will cause the algorithm to apply a weight
+% corresponding to the fraction of the season that has
+% passed. That is a game played when only 1/10 of the
+% season has elapsed will have 1/10 the weight of games
+% played on the last day of the season.
 
 % For detailed informaiton on the algorithm see
 % chapter four in
@@ -19,6 +26,8 @@
 % in your data without further adjustment. This works,
 % for example, with full season of NFL data.
 
+datetmpl = '%Y%m%d';
+
 args = argv();
 
 games = csvread(args{1});
@@ -28,6 +37,23 @@ fid = fopen(args{2}, 'r');
 teams = textscan(fid, '%*d,%s'){1};
 fclose(fid);
 
+if (length(args) == 4)
+  [seasonstart, nchars] = strptime(args{3}, datetmpl);
+  if (nchars == 0)
+    error('seasonstart does not parse.');
+  endif
+  seasonstart = mktime(seasonstart);
+  [seasonend, nchars] = strptime(args{4}, datetmpl);
+  if (nchars == 0)
+    error('seasonend does not parse.');
+  endif
+  seasonend = mktime(seasonend);
+  seasonlen = seasonend - seasonstart;
+  if (seasonlen <= 0)
+    error('Invalid seasonlen.')
+  end
+endif
+
 numteams = max(max(games(:,3)), max(games(:,6)));
 
 % Will hold the stat we are tracking.
@@ -36,14 +62,27 @@ A = zeros(numteams);
 G = zeros(numteams);
 
 for i = 1:rows(games)
+  gamestart = mktime(strptime(num2str(games(i, 2)), datetmpl));
+
   team1 = games(i, 3);
   team2 = games(i, 6);
 
   score1 = games(i, 5);
   score2 = games(i, 8);
 
-  A(team1, team2) += score1;
-  A(team2, team1) += score2;
+  weight = 1;
+
+  if (exist('seasonstart', 'var') == 1)
+    offset = gamestart - seasonstart;
+    if (offset < 0)
+      error('Game offset less than 0');
+    endif
+    weight = (gamestart - seasonstart) / seasonlen;
+  endif
+
+
+  A(team1, team2) += score1 * weight;
+  A(team2, team1) += score2 * weight;
   G(team1, team2) += 1;
   G(team2, team1) += 1;
 endfor
