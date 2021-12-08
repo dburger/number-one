300e51e63751773c82a165e0d517ed4ed6547cdc
David J. Burger <davidburger@gmail.com>
2021-12-07 21:16:48 -0700

    Add optional elapsed season weighting.
    Add optional weighting by the fraction of the season that has passed.

--- markov/markov.m
+++ markov/markov.m
@@ -8,16 +8,46 @@
 % columns 6 and 8. Outputs a single overall rating.
 % Example invocation:
 
-% markov.m scores.php teams.php
+% markov.m scores.php teams.php [seasonstart seasonend]
+
+% Providing seasonstart and seasonend, in the form of
+% yyyymmdd, will cause the algorithm to apply a weight
+% corresponding to the fraction of the season that has
+% passed. That is a game played when only 1/10 of the
+% season has elapsed will have 1/10 the weight of games
+% played on the last day of the season.
 
 % For detailed informaiton on the algorithm see
 % chapter six in
 % "Who's #1?: The Science of Rating and Ranking."
 
+datetmpl = '%Y%m%d';
+
 args = argv();
 
 games = csvread(args{1});
-teams = textread(args{2}, '%*d,%s');
+% Deprecated.
+% teams = textread(args{2}, '%*d,%s');
+fid = fopen(args{2}, 'r');
+teams = textscan(fid, '%*d,%s'){1};
+fclose(fid);
+
+if (length(args) == 4)
+  [seasonstart, nchars] = strptime(args{3}, datetmpl);
+  if (nchars == 0)
+    error('seasonstart does not parse.');
+  endif
+  seasonstart = mktime(seasonstart);
+  [seasonend, nchars] = strptime(args{4}, datetmpl);
+  if (nchars == 0)
+    error('seasonend does not parse.');
+  endif
+  seasonend = mktime(seasonend);
+  seasonlen = seasonend - seasonstart;
+  if (seasonlen <= 0)
+    error('Invalid seasonlen.')
+  end
+endif
 
 numteams = max(max(games(:,3)), max(games(:,6)));
 
@@ -27,26 +57,38 @@ S = zeros(numteams, numteams);
 S(:, :, 2) = zeros(numteams, numteams);
 
 for i = 1:rows(games)
+  gamestart = mktime(strptime(num2str(games(i, 2)), datetmpl));
+
   team1 = games(i, 3);
   team2 = games(i, 6);
 
   score1 = games(i, 5);
   score2 = games(i, 8);
 
+  weight = 1;
+
+  if (exist('seasonstart', 'var') == 1)
+    offset = gamestart - seasonstart;
+    if (offset < 0)
+      error('Game offset less than 0');
+    endif
+    weight = (gamestart - seasonstart) / seasonlen;
+  endif
+
   % In matrix 1, using wins, the loser "votes for" the winner.
   if (score1 > score2)
-    S(team2, team1, 1) += 1;
+    S(team2, team1, 1) += 1 * weight;
   elseif (score2 > score1)
-    S(team1, team2, 1) += 1;
+    S(team1, team2, 1) += 1 * weight;
   else
-    S(team2, team1, 1) += 0.5;
-    S(team1, team2, 1) += 0.5;
+    S(team2, team1, 1) += 0.5 * weight;
+    S(team1, team2, 1) += 0.5 * weight;
   endif
 
   % In matrix 2, using scores, a team votes for the other team
   % with how many points it gave up to that team.
-  S(team2, team1, 2) += score1;
-  S(team1, team2, 2) += score2;
+  S(team2, team1, 2) += score1 * weight;
+  S(team1, team2, 2) += score2 * weight;
 endfor
 
 % Normalize the rows.
